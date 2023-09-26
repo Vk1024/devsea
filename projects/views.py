36b78b52381bd6943698af497461582ed19becd2
vk0124 <geminianything@gmail.com>
2023-09-26 13:36:08 +0530

    working with templates

--- projects/views.py
+++ projects/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 
 
 #def projects(request):
-def projects(request):
-    return HttpResponse('Here is Our Products')
+def projects(request): 
+    #return HttpResponse('Here is Our Products')  #Simple Http Response
+    return render(request,'projects-1.html') # use render func to call templates
 
 #def project(request):
 def project(request, pk):
-    return HttpResponse('Here is Our rows and colmn' +  ' '  + str(pk))
+    #return HttpResponse('Here is Our rows and colmn' +  ' '  + str(pk)) #Simple Http Response
+    return render(request,'simple-projects-1.html') # use render func to call templates
+
 
 # Create your views here.
